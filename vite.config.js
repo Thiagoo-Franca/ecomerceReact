a244f790dd168a99149a42081148b76b5037ae62
Thiagoo-Franca <tfranca@ufba.br>
2024-12-23 09:11:04 -0300

    post 720px

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ecomerceReact/', // Adicione o nome do repositório aqui
+  base: '/ecomerceReact/', // Certifique-se de que este é o nome correto do repositório
 });
-
